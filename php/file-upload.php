7b57a16bc85e7d7ece7c442bfaad5628c20bb6c2
Ankit Katiyar <ankitkatiyar67@gmail.com>
2018-02-20 00:24:21 +0530

    remove unused code

--- php/file-upload.php
+++ php/file-upload.php
@@ -32,25 +32,9 @@ if(isset($_POST['submit']))
 
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="">
-	<meta name="author" content="">
+	<meta name="description" content="bytefold.com">
+	<meta name="author" content="bytefold">
 	<title>php File Upload</title>
-	<!-- Bootstrap Core CSS -->
-	<link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<!-- MetisMenu CSS -->
-	<link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
-	<!-- Custom CSS -->
-	<link href="../dist/css/sb-admin-2.css" rel="stylesheet">
-	<!-- Custom Fonts -->
-	<link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	<!-- php5 Shim and Respond.js IE8 support of php5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/php5shiv/3.7.0/php5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
 </head>
 
@@ -103,14 +87,4 @@ if(isset($_POST['submit']))
                             </div>
                         </div>
                     </div>
-
-                    <!-- jQuery -->
-<script src="../vendor/jquery/jquery.min.js"></script>
-<!-- Bootstrap Core JavaScript -->
-<script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-<!-- Metis Menu Plugin JavaScript -->
-<script src="../vendor/metisMenu/metisMenu.min.js"></script>
-<!-- Custom Theme JavaScript -->
-<script src="../dist/js/sb-admin-2.js"></script>
-
 </body>
